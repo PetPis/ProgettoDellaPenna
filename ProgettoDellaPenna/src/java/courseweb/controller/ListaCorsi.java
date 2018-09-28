d30fa47be4dcdc2d37d429217fbf518bd374550d
davidepetrini <davidepetrini@mbp-di-davide.homenet.telecomitalia.it>
2018-09-28 10:32:29 +0200

    docenti corsi

---
+++ ProgettoDellaPenna/src/java/courseweb/controller/ListaCorsi.java
@@ -0,0 +1,68 @@
+package courseweb.controller;
+
+import courseweb.view.FailureResult;
+import courseweb.view.TemplateManagerException;
+import courseweb.view.TemplateResult;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class ListaCorsi extends BaseController {
+
+    private void action_error(HttpServletRequest request, HttpServletResponse response) {
+        if (request.getAttribute("exception") != null) {
+            (new FailureResult(getServletContext())).activate((Exception) request.getAttribute("exception"), request, response);
+        } else {
+            (new FailureResult(getServletContext())).activate((String) request.getAttribute("message"), request, response);
+        }
+    }
+
+    private void action_default(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, TemplateManagerException {
+        TemplateResult res = new TemplateResult(getServletContext());
+        request.setAttribute("page_title", "Lista Corsi");
+        res.activate("courses_list.ftl.html", request, response);
+    }
+
+    
+    
+    @Override
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException {
+            //boolean b=false;
+        try {
+            /*Enumeration list=request.getParameterNames();
+            while(list.hasMoreElements())
+                if(list.nextElement().toString().equals("lin")){
+                    b=true;
+                    break;
+                }
+            if(b)
+                lin=request.getParameter("lin");
+            lin="it";
+            */
+            /*if(request.getParameter("lin")==null)
+                lin="it";
+            else
+                lin=request.getParameter("lin");*/
+            action_default(request, response);
+
+        } catch (IOException | TemplateManagerException ex) {
+            request.setAttribute("exception", ex);
+            action_error(request, response);
+
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Main Corso servlet";
+    }// </editor-fold>
+
+}
